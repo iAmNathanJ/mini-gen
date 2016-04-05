b52ae1101198a973362c0f83862c866c1b5ba605
iAmNathanJ <jacobs@heysparkbox.com>
2016-04-05 15:04:39 -0400

    testing workflow resolved

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let test = require('tape');
+let MiniGen = require('../');
+
+test('MiniGen is a thing', t => {
+  t.ok(MiniGen, 'It exists!');
+  t.end();
+});
+
+test('MiniGen', t => {
+  t.equal();
+  t.end();
+});
